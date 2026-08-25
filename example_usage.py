b599efed482d95db2adae4012fe18c31d127d5e3
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-08-25 20:54:22 +0800

    fix: syntax formatting in example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -5,8 +5,8 @@ def main():
     res = client.predict_multi_line_code_completion('def verify_jwt_signature_rsa256(token, public_key_pem):', True)
     print('Completion ID: ' + res['completion_id'] + ' (Latency: ' + str(res['prediction_latency_ms']) + 'ms)')
     print('Air-Gapped: ' + str(res['air_gapped_isolated']) + ' | Acceptance Score: ' + str(res['acceptance_probability_score_pct']) + '%')
-    print('Code:
-' + res['predicted_code_block'])
+    print('Predicted Code:')
+    print(res['predicted_code_block'])
 
 if __name__ == '__main__':
     main()
